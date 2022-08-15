4a1f8677591da90e66d99ff0000ffc8a6514f18c
SaulLu <lucilesaul.com@gmail.com>
2022-08-15 21:18:57 +0000

    exclude broken file

--- first_time_load.py
+++ first_time_load.py
@@ -4,7 +4,7 @@ from load_dataset import load_dataset_by_files, all_files
 
 def main(args):
     dataset_path = args.dataset_path 
-    files = [file[:-len(".gz")] for file in all_files if file.endswith(".jsonl.gz")]
+    files = [file[:-len(".gz")] for file in all_files if file.endswith(".jsonl.gz") and file not in args.files_to_exclude]
     print(f"{len(files)} files are included")
 
     ds = load_dataset_by_files(files=files, dataset_name_or_path=dataset_path)
@@ -14,6 +14,7 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Load a dataset.')
     parser.add_argument('--dataset_path', type=str, default='c4-en-html-with-metadata')
+    parser.add_argument('--files_to_exclude', nargs='+', default=[])
     args = parser.parse_args()
 
     main(args)
