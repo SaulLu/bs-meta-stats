509e4f37c4e00543943338284b2118475c4b00c7
SaulLu <lucilesaul.com@gmail.com>
2022-08-15 21:51:51 +0000

    get num meta

--- explore.py
+++ explore.py
@@ -1,18 +1,18 @@
 #%%
 from collections import Counter, defaultdict
-from load_dataset import load_dataset_by_files, all_files
+from load_dataset import load_dataset_by_files
 #%%
-# files = [
-#     "c4-en-html_cc-main-2019-18_pq00-000.jsonl.gz",
-#     "c4-en-html_cc-main-2019-18_pq00-001.jsonl.gz",
-#     "c4-en-html_cc-main-2019-18_pq00-002.jsonl.gz",
-#     "c4-en-html_cc-main-2019-18_pq00-003.jsonl.gz",
-#     "c4-en-html_cc-main-2019-18_pq00-004.jsonl.gz",
-# ]
+files = [
+    "c4-en-html_cc-main-2019-18_pq00-000.jsonl",
+    "c4-en-html_cc-main-2019-18_pq00-001.jsonl",
+    "c4-en-html_cc-main-2019-18_pq00-002.jsonl",
+    "c4-en-html_cc-main-2019-18_pq00-003.jsonl",
+    "c4-en-html_cc-main-2019-18_pq00-004.jsonl",
+]
 # %%
 dataset_path = '/home/lucile_huggingface_co/data/c4-en-html-with-metadata'
 # %%
-ds = load_dataset_by_files(files=all_files, dataset_name_or_path=dataset_path)
+ds = load_dataset_by_files(files=files, dataset_name_or_path=dataset_path)
 # %%
 ds
 # %%
@@ -129,4 +129,23 @@ for i in range(1000):
     if ds[i]["metadata_timestamp"] != []:
         print(i, ds[i]["metadata_timestamp"][0]["value"])
         # break
+# %%
+metadata_names = [name for name in ds.features.keys() if name.startswith("metadata_")] 
+# %%
+metadata_names
+# %%
+for name in metadata_names:
+    print(name, type(ds[0][name]))
+# %%
+def is_not_empty_list(values):
+    return {"not_none": list(filter(lambda v: v!=[], values))}
+
+def get_num(ds, metadata_col_name, num_proc):
+    column_names = list(ds.features.keys())
+    sub_ds = ds.map(is_not_empty_list, batched=True, num_proc=num_proc, remove_columns=column_names, input_columns=[metadata_col_name])
+    return len(sub_ds)
+# %%
+
+for name in metadata_names:
+    print(name, get_num(ds, metadata_col_name=name, num_proc=16))
 # %%
